dc9b96b2a60e98aa90c2c424f288ac7adf30993e
hank <hancockgu1@gmail.com>
2024-12-22 22:42:50 +0800

    add error page

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 # 注册蓝图
 from puppycompanyblog.core.views import core
+from puppycompnayblog.error_pages.handler import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
